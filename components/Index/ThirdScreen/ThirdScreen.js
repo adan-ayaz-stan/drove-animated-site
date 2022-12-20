db7dfa106e0b7b3b70e3c4095469fff9d60d17e3
Adan Ayaz <999a.d.a.n.a.y.a.z999@gmail.com>
2022-12-20 18:56:57 +0500

    Add files via upload

--- components/Index/ThirdScreen/ThirdScreen.js
+++ components/Index/ThirdScreen/ThirdScreen.js
@@ -1,5 +1,60 @@
-import styles from './main.module.css';
+import styles from "./main.module.css";
+
+import bulbSvg from "../../../cms/svgs/bulb.svg";
+import vectorSvg from "../../../cms/svgs/vector.svg";
+import stackSvg from "../../../cms/svgs/stack.svg";
+import Image from "next/image";
 
 export default function ThirdScreen() {
-    return <div className={styles.main}></div>
+  const sampleArray = [
+    {
+      img: bulbSvg,
+      heading: "Digital Products",
+      description:
+        "Web design encompasses many different skills and disciplines in the production of all web.",
+    },
+    {
+      img: vectorSvg,
+      heading: "UI-UX Design",
+      description:
+        "Web design encompasses many different skills and disciplines in the production of all web.",
+    },
+    {
+      img: stackSvg,
+      heading: "Web Development",
+      description:
+        "Web design encompasses many different skills and disciplines in the production of all web.",
+    },
+  ];
+
+  return (
+    <div className={styles.main}>
+      <h4 className={styles.small_heading}>
+        {"•"}
+        <div
+          style={{ height: "1px", width: "100px", backgroundColor: "black" }}
+        />
+        Our Services
+      </h4>
+
+      <h1 className={styles.large_heading}>New Branding Agency</h1>
+
+      <div className={styles.cards_container}>
+        {sampleArray.map((ele, ind) => {
+          return (
+            <div
+              className={styles.card}
+              style={ind == 1 ? { backgroundColor: "rgb(10, 10, 10)" } : {}}
+            >
+              <div className={styles.card_image}>
+                <Image src={ele.img} alt="card-image" layout="fill" />
+              </div>
+              <h2>{ele.heading}</h2>
+              <p>{ele.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
